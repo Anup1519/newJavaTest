2966ca47d9e48e8934a24c6441bd1c5e59249746
Anup1519 <anupraj1519@gmail.com>
2021-12-24 18:41:43 +0530

    Variables

--- src/variables/Sample.java
+++ src/variables/Sample.java
@@ -3,6 +3,10 @@ package variables;
 import methods.Example;
 
 public class Sample {
+
+    static void m1(){
+        System.out.println("m1");
+    }
     public static void main(String[] args) {
         int a=40;
         System.out.println("a:"+a);
@@ -15,5 +19,12 @@ public class Sample {
 
         e1=new Example();
         System.out.println("e1:"+e1);  //  O/P: e1:methods.Example@74a14482
+
+        int y=30;
+        System.out.println(y); // o/p: 30 variables calling stmt
+
+        m1(); //Variables calling stmt
+
+
     }
 }
